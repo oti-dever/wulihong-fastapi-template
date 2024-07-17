026dcac2ccc4737caed5c17cc105bf805b7cc639
Sun-ZhenXing <1006925066@qq.com>
2024-07-17 17:44:26 +0800

    WIP: add structures

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # API 服务模板（FastAPI）
 
+本项目是一个基于 FastAPI 的 API 服务模板。
+
+> 本项目使用 LF 格式行尾，请确保你的编辑器支持，推荐使用 VS Code。全局设置停止自动转换行尾：
+>
+> ```bash
+> git config --global core.autocrlf false
+> ```
+
+克隆项目：
+
+```bash
+git clone https://github.com/oti-dever/wulihong-fastapi-template.git
+cd wulihong-fastapi-template
+```
+
 创建虚拟环境：
 
 ```bash
@@ -26,7 +41,7 @@ fastapi dev main.py --reload
 uvicorn main:app --reload
 ```
 
-运行：
+生产运行：
 
 ```bash
 uvicorn main:app
@@ -40,8 +55,12 @@ pre-commit run --all-files
 
 ## 部署
 
-编辑 `.env.production` 文件配置环境变量，然后运行：
+本项目使用 Docker 多阶段构建镜像，并生成为动态链接库（共享库）来运行，一定程度上可保护源码并加快程序运行速度，但可能造成部分第三方库工作不正常。
+
+根据 `.env.example` 编辑 `.env.production` 文件配置环境变量，然后运行：
 
 ```bash
 docker compose --env-file .env.production up -d
 ```
+
+这将自动构建镜像并运行容器。
--- setup.py
+++ setup.py
@@ -1,16 +1,31 @@
 from distutils.core import setup
 
+import toml
 from Cython.Build import cythonize
 from Cython.Compiler import Options
 
 Options.docstrings = False
 
+with open("requirements.txt", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
+with open("pyproject.toml", encoding="utf-8") as f:
+    pyproject = toml.load(f)
+    poetry_config = pyproject["tool"]["poetry"]
+
+with open("README.md", encoding="utf-8") as f:
+    readme = f.read()
+
 setup(
-    name="fastapi_server",
+    name=poetry_config["name"],
+    version=poetry_config["version"],
+    description=poetry_config["description"],
+    long_description=readme,
+    author=poetry_config["authors"][0],
     ext_modules=cythonize(
         [
             "main.py",
-            "src/**/*.py",
+            "app/**/*.py",
         ],
         compiler_directives={"language_level": 3},
     ),
